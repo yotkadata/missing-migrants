846fff3b39c6209a7ace97d3ab356b9e3dc7c4d8
Jan Kühn <yotka@posteo.org>
2023-09-21 22:37:05 +0200

    Add date to threshold

--- src/components/Thresholds.svelte
+++ src/components/Thresholds.svelte
@@ -2,6 +2,7 @@
   export let xScale;
 
   import thresholds from "$data/data-migration-thresholds.json";
+  import { timeFormat } from "d3";
 
   // Convert date strings to Date objects
   thresholds.forEach((d) => {
@@ -12,13 +13,19 @@
   const formatNumber = new Intl.NumberFormat("en-US", {
     style: "decimal",
   }).format;
+
+  // Function to format dates
+  const formatDate = timeFormat("%b %Y");
 </script>
 
 <g class="thresholds">
   {#each thresholds as threshold}
     <g class="threshold" transform="translate({xScale(threshold.date)}, 0)">
-      <text x="3" y="12">{formatNumber(threshold.threshold)}</text>
-      <line x1="0" x2="0" y1="0" y2="30" stroke="red" />
+      <text class="number" x="3" y="12"
+        >{formatNumber(threshold.threshold)}</text
+      >
+      <text class="date" x="3" y="27">{formatDate(threshold.date)}</text>
+      <line x1="0" x2="0" y1="0" y2="40" stroke="#fff" />
     </g>
   {/each}
 </g>
@@ -26,7 +33,14 @@
 <style>
   .thresholds text {
     font-family: Lato, sans-serif;
-    fill: rgba(255, 255, 255, 0.3);
+  }
+  .thresholds .number {
+    fill: rgba(255, 255, 255, 0.8);
     font-size: 1rem;
+    font-weight: 700;
+  }
+  .thresholds .date {
+    fill: rgba(255, 255, 255, 0.3);
+    font-size: 0.8rem;
   }
 </style>
