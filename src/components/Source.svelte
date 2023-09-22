da017e0637be60d27fd707dc503d53458e62d06d
Jan Kühn <yotka@posteo.org>
2023-09-22 13:30:18 +0200

    Move Source to own component

---
+++ src/components/Source.svelte
@@ -0,0 +1,24 @@
+<div class="source">
+  <p>
+    <strong>Graph:</strong> Jan Kühn,
+    <a href="https://yotka.org">https://yotka.org</a> <strong>Data:</strong>
+    <a href="https://missingmigrants.iom.int/">Missing Migrants Project</a>
+    <strong>License:</strong>
+    <a href="http://creativecommons.org/licenses/by/4.0/">CC by-sa-nd 4.0</a>
+  </p>
+</div>
+
+<style>
+  div.source {
+    font-size: 0.8rem;
+    padding-top: 1rem;
+  }
+  div.source a,
+  div.source a:visited {
+    color: rgba(255, 255, 255, 0.8);
+    text-decoration: none;
+  }
+  div.source strong {
+    font-weight: 800;
+  }
+</style>
