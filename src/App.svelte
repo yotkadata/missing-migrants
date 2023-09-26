f1937c05b2fb741b3949cb913f748cef27db58e9
Jan Kühn <yotka@posteo.org>
2023-09-26 13:19:51 +0200

    Remove precalculated positions

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,5 @@
 <script>
-  import dataRaw from "$data/data-migration-incidents-precomputed.json";
-  //import data from "$data/data-migration-individual.json";
+  import dataRaw from "$data/data-migration-incidents.json";
   import totals from "$data/data-migration-totals.json";
 
   // Convert date strings to Date objects
